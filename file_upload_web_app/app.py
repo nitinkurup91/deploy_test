c4181c0bda648b177c3633667359bee4260b5c95
NitinKurup <47655265+nitinkurup91@users.noreply.github.com>
2023-11-04 14:57:52 +0530

    Update app.py

--- file_upload_web_app/app.py
+++ file_upload_web_app/app.py
@@ -18,7 +18,7 @@ def upload():
                 return render_template("index.html",msg =msg)
 
 if __name__ == "__main__":
-    #app.run(host="0.0.0.0",port=8080) # EC2 on AWS
-    app.run(host="127.0.0.1",port=8080) # local machine
+    app.run(host="0.0.0.0",port=8080) # EC2 on AWS
+    #app.run(host="127.0.0.1",port=8080) # local machine
     # app.run(host="172.25.12.70",port=8080) # local machine
     
